3adceec1700c4009355545dfefa8e9970d55d0f5
Rafifou64 <126586065+Rafifou64@users.noreply.github.com>
2024-01-03 16:58:04 +0100

    debut page listing reservation

--- src/application/controller/ListeReservationController.java
+++ src/application/controller/ListeReservationController.java
@@ -2,18 +2,18 @@ package application.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import application.entite.Reservation;
 import application.service.MainService;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 
 public class ListeReservationController implements Initializable {
 	@FXML
@@ -25,11 +25,24 @@ public class ListeReservationController implements Initializable {
 	@FXML
 	private Label labSalle;
 	
+	@FXML
+	private Label labDate;
+	
+	@FXML
+	private DatePicker datePicker;
+	
+	private LocalDate dateReservation;
+	
+	private ArrayList<Reservation> lstReservation;
+	
 	private MainService mainService;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		this.mainService = new MainService();		
+		this.mainService = new MainService();
+		this.setDateToday(this.mainService.getDateNow());
+		this.dateReservation = LocalDate.now();
+		this.datePicker.setValue(dateReservation);		
 	}
 	
 	public void goToAccueil(MouseEvent e) throws IOException
@@ -45,6 +58,22 @@ public class ListeReservationController implements Initializable {
 	public void goToSalle(MouseEvent e) throws IOException
 	{
 		this.mainService.navigateTo(e, "../vue/Salle.fxml");
+	}
+	
+	public void setDateToday(String date)
+	{
+		labDate.setText("  Réservation du " + date);
+	}
+	
+	public void changeDateReservation()
+	{
+		this.dateReservation = this.datePicker.getValue();
+		this.setDateToday(this.mainService.formatDate(this.dateReservation));
+	}
+	
+	public void getReservation()
+	{
+		
 	}
 
 	
--- src/application/service/MainService.java
+++ src/application/service/MainService.java
@@ -1,15 +1,31 @@
 package application.service;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.ResourceBundle;
 
+import application.entite.Reservation;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class MainService {
+public class MainService implements Initializable {
+	
+	private DAO dao;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.dao = new DAO();
+	}
 	
 	public void navigateTo(MouseEvent e, String vue) throws IOException
 	{
@@ -27,6 +43,35 @@ public class MainService {
 			stage.setScene(sceneListeReservationFXML);
 
 			stage.show();
+	}
+	
+	public String getDateNow() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(formatter);
+    }
+	
+	public String formatDate(LocalDate date)
+	{
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(formatter);
+	}
+	
+	public ArrayList<Reservation> getReservationByDate(LocalDate dateReservation)
+	{
+		Date date = this.convertLocalDateToDate(dateReservation);
+		return this.dao.getAllReservationByDate(date);
+	}
+	
+	public Date convertLocalDateToDate(LocalDate localDate)
+	{
+		//default time zone
+		ZoneId defaultZoneId = ZoneId.systemDefault();
+        
+        //local date + atStartOfDay() + default time zone + toInstant() = Date
+        Date date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+        
+        return date;
 	}
 
 }

6fffe4f24ad40a9ff1d5ef38acc48bfe42bd87a3
Rafifou64 <126586065+Rafifou64@users.noreply.github.com>
2024-01-23 11:36:47 +0100

    Ajout des assiettes sur la page salle

--- src/application/controller/SalleController.java
+++ src/application/controller/SalleController.java
@@ -77,16 +77,111 @@ public class SalleController implements Initializable {
 	private ImageView table_3_5;
 	
 	@FXML
+	private ImageView plate_1_1_1;
+	
+	@FXML
+	private ImageView plate_1_1_2;
+	
+	@FXML
+	private ImageView plate_1_2_1;
+	
+	@FXML
+	private ImageView plate_1_2_2;
+	
+	@FXML
+	private ImageView plate_1_3_1;
+	
+	@FXML
+	private ImageView plate_1_3_2;
+	
+	@FXML
+	private ImageView plate_1_4_1;
+	
+	@FXML
+	private ImageView plate_1_4_2;
+	
+	@FXML
+	private ImageView plate_1_5_1;
+	
+	@FXML
+	private ImageView plate_1_5_2;
+	
+	@FXML
+	private ImageView plate_2_1_1;
+	
+	@FXML
+	private ImageView plate_2_1_2;
+	
+	@FXML
+	private ImageView plate_2_2_1;
+	
+	@FXML
+	private ImageView plate_2_2_2;
+	
+	@FXML
+	private ImageView plate_2_3_1;
+	
+	@FXML
+	private ImageView plate_2_3_2;
+	
+	@FXML
+	private ImageView plate_2_4_1;
+	
+	@FXML
+	private ImageView plate_2_4_2;
+	
+	@FXML
+	private ImageView plate_2_5_1;
+	
+	@FXML
+	private ImageView plate_2_5_2;
+	
+	@FXML
+	private ImageView plate_3_1_1;
+	
+	@FXML
+	private ImageView plate_3_1_2;
+	
+	@FXML
+	private ImageView plate_3_2_1;
+	
+	@FXML
+	private ImageView plate_3_2_2;
+	
+	@FXML
+	private ImageView plate_3_3_1;
+	
+	@FXML
+	private ImageView plate_3_3_2;
+	
+	@FXML
+	private ImageView plate_3_4_1;
+	
+	@FXML
+	private ImageView plate_3_4_2;
+	
+	@FXML
+	private ImageView plate_3_5_1;
+	
+	@FXML
+	private ImageView plate_3_5_2;
+	
+	@FXML
 	private DatePicker datePicker;
 	
 	@FXML
 	private Label labDate;
+	
+	@FXML
+	private Label labMessage;
 	
 	private LocalDate dateReservation;
 	
 	private ArrayList<Reservation> lstReservation;
 	
 	private ArrayList<ImageView> lstTableDisplay;
+	
+	private ArrayList<ImageView> lstPlateDisplay;
 	
 	private MainService mainService;
 	
@@ -98,6 +193,7 @@ public class SalleController implements Initializable {
 		this.datePicker.setValue(dateReservation);
 		this.getReservation();
 		this.initListeTable();
+		this.initListePlate();
 		this.displayTable();
 	}
 	
@@ -136,6 +232,41 @@ public class SalleController implements Initializable {
 		this.lstTableDisplay.add(table_3_5);
 	}
 	
+	public void initListePlate()
+	{
+		this.lstPlateDisplay = new ArrayList<ImageView>();
+		this.lstPlateDisplay.add(plate_1_1_1);
+		this.lstPlateDisplay.add(plate_1_1_2);
+		this.lstPlateDisplay.add(plate_1_2_1);
+		this.lstPlateDisplay.add(plate_1_2_2);
+		this.lstPlateDisplay.add(plate_1_3_1);
+		this.lstPlateDisplay.add(plate_1_3_2);
+		this.lstPlateDisplay.add(plate_1_4_1);
+		this.lstPlateDisplay.add(plate_1_4_2);
+		this.lstPlateDisplay.add(plate_1_5_1);
+		this.lstPlateDisplay.add(plate_1_5_2);
+		this.lstPlateDisplay.add(plate_2_1_1);
+		this.lstPlateDisplay.add(plate_2_1_2);
+		this.lstPlateDisplay.add(plate_2_2_1);
+		this.lstPlateDisplay.add(plate_2_2_2);
+		this.lstPlateDisplay.add(plate_2_3_1);
+		this.lstPlateDisplay.add(plate_2_3_2);
+		this.lstPlateDisplay.add(plate_2_4_1);
+		this.lstPlateDisplay.add(plate_2_4_2);
+		this.lstPlateDisplay.add(plate_2_5_1);
+		this.lstPlateDisplay.add(plate_2_5_2);
+		this.lstPlateDisplay.add(plate_3_1_1);
+		this.lstPlateDisplay.add(plate_3_1_2);
+		this.lstPlateDisplay.add(plate_3_2_1);
+		this.lstPlateDisplay.add(plate_3_2_2);
+		this.lstPlateDisplay.add(plate_3_3_1);
+		this.lstPlateDisplay.add(plate_3_3_2);
+		this.lstPlateDisplay.add(plate_3_4_1);
+		this.lstPlateDisplay.add(plate_3_4_2);
+		this.lstPlateDisplay.add(plate_3_5_1);
+		this.lstPlateDisplay.add(plate_3_5_2);
+	}
+	
 	public void setDateToday(String date)
 	{
 		labDate.setText("  Réservation du " + date);
@@ -154,20 +285,44 @@ public class SalleController implements Initializable {
 		this.displayTable();
 	}
 	
+	public void setDisplayDefault()
+	{
+		this.labMessage.setText("");
+		for (int i = 0; i < this.lstTableDisplay.size(); i++)
+			this.lstTableDisplay.get(i).visibleProperty().set(false);
+		
+		for (int i = 0; i < this.lstPlateDisplay.size(); i++)
+			this.lstPlateDisplay.get(i).visibleProperty().set(false);
+	}
+	
 	public void displayTable()
 	{
-		for (int i = 0; i < this.lstTableDisplay.size(); i++) {
-			this.lstTableDisplay.get(i).visibleProperty().set(false);
-		}
+		this.setDisplayDefault();
 		
-		int nbTable = 0;
+		int cptTable = 0;
+		int nbTableReservation = 0;
 		for (int i = 0; i < lstReservation.size(); i++) {
 			Reservation reservation = lstReservation.get(i);
+			int nbPersonne = reservation.getNbpersonne();
+			nbTableReservation = nbTableReservation + reservation.getLstTables().size();
 			for (int j = 0; j < reservation.getLstTables().size(); j++) {
-				this.lstTableDisplay.get(nbTable).visibleProperty().set(true);
-				nbTable++;
+				this.displayPlate(nbPersonne, cptTable*2);
+				this.lstTableDisplay.get(cptTable).visibleProperty().set(true);
+				nbPersonne = nbPersonne - 2;
+				cptTable++;
 			}
-			nbTable++;
+			cptTable++;
+		}
+		
+		if(nbTableReservation <= 0)
+			this.labMessage.setText("Il n'y a pas de table réservée pour cette date et horaire");
+	}
+	
+	public void displayPlate(int nbPersonne, int cptPlate)
+	{
+		for (int i = 0; (i < nbPersonne && i < 2); i++) {
+			this.lstPlateDisplay.get(cptPlate).visibleProperty().set(true);
+			cptPlate++;
 		}
 	}
 }
